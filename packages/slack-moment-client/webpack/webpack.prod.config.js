b4ba9b15c868eeff5eebddca1fce4ff53c3ad4ba
ykhs <yukihisa.yamada@gmail.com>
2017-10-14 21:35:24 +0900

    refactor webpack config

---
+++ packages/slack-moment-client/webpack/webpack.prod.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const baseWebpackConfig = require('./webpack.base.config');
+
+const envPath = path.resolve(path.join('..', '..', '..', '.env'));
+
+require('dotenv').config({
+  path: envPath
+});
+
+module.exports = merge(baseWebpackConfig, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+        FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+        FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL)
+      }
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ]
+});

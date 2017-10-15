7493aca491b24aa5594fa2a569d2abf506c03151
ykhs <yukihisa.yamada@gmail.com>
2017-10-15 22:01:30 +0900

    clean

--- packages/slack-moment-client/src/js/styles.js
+++ packages/slack-moment-client/src/js/styles.js
@@ -2,4 +2,3 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'nprogress/nprogress.css';
 import 'hover.css/css/hover.css';
 import 'font-awesome/css/font-awesome.css';
-import '../css/main.css';

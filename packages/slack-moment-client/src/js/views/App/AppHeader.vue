358d265d01b18f719f9983752808637130aa4901
ykhs <yukihisa.yamada@gmail.com>
2017-10-15 22:58:50 +0900

    styles before signin

--- packages/slack-moment-client/src/js/views/App/AppHeader.vue
+++ packages/slack-moment-client/src/js/views/App/AppHeader.vue
@@ -1,35 +1,29 @@
 <template>
-  <header class="header">
-    <nav class="navbar navbar-light">
-      <div class="container">
-        <div class="d-flex justify-content-end">
-          <div class="mr-auto">
-            <router-link class="navbar-brand" to="/">
-              Slack Moment
-            </router-link>
-          </div>
-          <div>
-            <template v-if="user.name">
-              <span class="navbar-text mr-3">
-                Signed in as {{ user.name }}
-              </span>
-              <a class="btn btn-sm align-middle btn-outline-secondary" href="#" @click.prevent="signOut">
-                Sign out
-              </a>
-            </template>
+  <header class="SM-AppHeader">
+    <div class="SM-AppHeader__container d-flex px-3 justify-content-between container">
 
-            <template v-if="needShowSignIn">
-              <span class="navbar-text">
-                <i class="fa fa-sign-in" aria-hidden="true"></i>
-                <!-- <a class="btn btn-sm align-middle btn-outline-success" href="/auth/slack">
-                  Sign in
-                </a> -->
-              </span>
-            </template>
-          </div>
-        </div>
+      <router-link class="SM-AppHeader__logo navbar-brand" to="/">
+        Slack Moment
+      </router-link>
+
+      <div class="SM-AppHeader__menu">
+        <template v-if="user.name">
+          <span class="navbar-text mr-3">
+            Signed in as {{ user.name }}
+          </span>
+          <a class="btn btn-sm align-middle btn-outline-secondary" href="#" @click.prevent="signOut">
+            Sign out
+          </a>
+        </template>
+
+        <template v-if="needShowSignIn">
+          <a class="SM-AppHeader__signIn" href="/auth/slack">
+            <i class="fa fa-sign-in" aria-hidden="true"></i>
+          </a>
+        </template>
       </div>
-    </nav>
+
+    </div>
   </header>
 </template>
 
@@ -45,10 +39,19 @@ export default {
 </script>
 
 <style scoped>
-.header {
+.SM-AppHeader {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  color: rgba(255, 255, 255, 0.8);
   background-color: #00C9B7;
 }
-.navbar-brand {
+.SM-AppHeader__logo {
+  line-height: 1;
+  color: rgba(255, 255, 255, 0.8);
   font-weight: 700;
+}
+.SM-AppHeader__signIn {
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 1.5em;
 }
 </style>

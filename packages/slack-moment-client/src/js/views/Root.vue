af026cca76f191ebe2131cdc1973fad55507c75b
ykhs <yukihisa.yamada@gmail.com>
2017-10-16 13:01:31 +0900

    root styling

--- packages/slack-moment-client/src/js/views/Root.vue
+++ packages/slack-moment-client/src/js/views/Root.vue
@@ -1,55 +1,61 @@
 <template>
   <main class="main">
+    <v-content>
+      <v-container>
 
-    <div class="container">
-      <div class="card">
-        <div class="card-block p-5">
-          <p class="card-title">
-            Slack Momentは、Slackのログをまとめて書き出すサービスです。
-          </p>
-          <p class="card-text">
-            まとめたログはMarkdownとして出てくるので、適当な場所に貼って残すといいと思います。
-          </p>
+        <template v-if="needShowSignIn">
+          <v-card>
+            <v-card-title primary-title>
+              <div class="title">
+                Slack Momentは、Slackのログをまとめて書き出すサービスです。
+              </div>
+            </v-card-title>
 
-          <template v-if="needShowSignIn">
-            <hr class="my-4">
+            <v-card-text>
+              <div>
+                まとめたログはMarkdownとして出てくるので、適当な場所に貼って残すといいと思います。
+              </div>
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-text>
+              <div>
+                Slackアカウントでログインしてください。
+              </div>
+              <v-btn color="cyan" href="/auth/slack">
+                <v-icon left>fa-slack</v-icon>
+                Slackアカウントでログイン
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </template>
+
+        <template v-if="user.name">
+          <div class="d-flex align-items-end">
+            <div class="mr-2">
+              <img class="rounded" :src="user.image_72">
+            </div>
+            <div>
+              <span>
+                {{ user.name }} としてログイン中です。
+              </span>
+            </div>
+          </div>
+
+          <div class="mt-2">
             <p>
-              Slackアカウントでログインしてください。
+              まとめたいログがあるチャンネルを探しましょう。<br />
+              次のボタンを押すと、あなたが参加しているチャンネル一覧が表示されます。
             </p>
-            <a href="/auth/slack" class="btn btn-info">
-              <i class="fa fa-lg fa-slack" aria-hidden="true"></i>
-              Slackアカウントでログイン
-            </a>
-          </template>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <template v-if="user.name">
-        <div class="d-flex align-items-end">
-          <div class="mr-2">
-            <img class="rounded" :src="user.image_72">
+            <router-link to="channels" class="btn btn-secondary btn-lg btn-block">
+              チャンネル一覧を見る
+            </router-link>
           </div>
-          <div>
-            <span>
-              {{ user.name }} としてログイン中です。
-            </span>
-          </div>
-        </div>
+        </template>
 
-        <div class="mt-2">
-          <p>
-            まとめたいログがあるチャンネルを探しましょう。<br />
-            次のボタンを押すと、あなたが参加しているチャンネル一覧が表示されます。
-          </p>
-          <router-link to="channels" class="btn btn-secondary btn-lg btn-block">
-            チャンネル一覧を見る
-          </router-link>
-        </div>
-      </template>
-    </div>
+      </v-container>
+    </v-content>
   </main>
 </template>
 

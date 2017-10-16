84fe4e7cdb87df3120cfa1291145e5c904390311
ykhs <yukihisa.yamada@gmail.com>
2017-10-16 14:20:19 +0900

    duplicate component

---
+++ packages/slack-moment-client/src/js/components/Introduction.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="pa-4">
+    <v-card-title primary-title>
+      <div class="title">
+        Slack Momentは、Slackのログをまとめて書き出すサービスです。
+      </div>
+    </v-card-title>
+
+    <v-card-text>
+      <div>
+        まとめたログはMarkdownとして出てくるので、適当な場所に貼って残すといいと思います。
+      </div>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-text>
+      <div>
+        Slackアカウントでログインしてください。
+      </div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="cyan darken-1" class="white--text" href="/auth/slack">
+        <v-icon left>fa-slack</v-icon>
+        Slackアカウントでログイン
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Introduction'
+}
+</script>
--- packages/slack-moment-client/src/js/views/Root.vue
+++ packages/slack-moment-client/src/js/views/Root.vue
@@ -2,56 +2,21 @@
   <main class="main">
     <v-content>
       <v-container>
+        <introduction v-if="needShowSignIn"></introduction>
 
-        <template v-if="needShowSignIn">
-          <v-card>
+        <template v-if="user.name">
+          <v-breadcrumbs divider=">">
+            <v-breadcrumbs-item disabled>ホーム</v-breadcrumbs-item>
+            <v-breadcrumbs-item disabled>チャンネル一覧</v-breadcrumbs-item>
+          </v-breadcrumbs>
+
+          <v-card class="pa-4">
             <v-card-title primary-title>
               <div class="title">
                 Slack Momentは、Slackのログをまとめて書き出すサービスです。
               </div>
             </v-card-title>
-
-            <v-card-text>
-              <div>
-                まとめたログはMarkdownとして出てくるので、適当な場所に貼って残すといいと思います。
-              </div>
-            </v-card-text>
-
-            <v-divider></v-divider>
-
-            <v-card-text>
-              <div>
-                Slackアカウントでログインしてください。
-              </div>
-              <v-btn color="cyan" href="/auth/slack">
-                <v-icon left>fa-slack</v-icon>
-                Slackアカウントでログイン
-              </v-btn>
-            </v-card-text>
           </v-card>
-        </template>
-
-        <template v-if="user.name">
-          <div class="d-flex align-items-end">
-            <div class="mr-2">
-              <img class="rounded" :src="user.image_72">
-            </div>
-            <div>
-              <span>
-                {{ user.name }} としてログイン中です。
-              </span>
-            </div>
-          </div>
-
-          <div class="mt-2">
-            <p>
-              まとめたいログがあるチャンネルを探しましょう。<br />
-              次のボタンを押すと、あなたが参加しているチャンネル一覧が表示されます。
-            </p>
-            <router-link to="channels" class="btn btn-secondary btn-lg btn-block">
-              チャンネル一覧を見る
-            </router-link>
-          </div>
         </template>
 
       </v-container>
@@ -64,12 +29,34 @@ import {
   mapState,
   mapGetters
 } from 'vuex'
+import Introduction from '../components/Introduction.vue';
 
 export default {
   name: 'Root',
   computed: {
     ...mapState(['user']),
     ...mapGetters(['needShowSignIn'])
+  },
+  components: {
+    Introduction
+  },
+  data () {
+    return {
+      items: [
+        {
+          text: 'Dashboard',
+          disabled: false
+        },
+        {
+          text: 'Link 1',
+          disabled: false
+        },
+        {
+          text: 'Link 2',
+          disabled: true
+        }
+      ]
+    }
   }
 }
 </script>
